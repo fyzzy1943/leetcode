110ed2c621804fba7ba21ac3d0277881b92d4186
forDawn <fyzzy1943@gmail.com>
2017-12-30 16:04:50 +0800

    done.

--- 746. Min Cost Climbing Stairs.py
+++ 746. Min Cost Climbing Stairs.py
@@ -4,32 +4,38 @@ class Solution:
         :type cost: List[int]
         :rtype: int
         """
-        n = len(cost)-1
-        def costSum(i, j):
-            if i == n or i == n-1:
-                return cost[i]
+        # n = len(cost)-1
+        # def costSum(i, j):
+        #     if i == n or i == n-1:
+        #         return cost[i]
+        #
+        #     if cost[i] < cost[j]:
+        #         return cost[i] + costSum(i+2, j+2)
+        #     else:
+        #         return cost[j] + costSum(i + 2, j + 2)
+        # sum = 0
+        #
+        # while True:
+        #     if len(cost) == 0:
+        #         break
+        #     if len(cost) == 1:
+        #         sum += cost.pop(0)
+        #         break
+        #     left = cost.pop(0)
+        #     right = cost.pop(1)
+        #
+        #
+        # return sum
 
-            if cost[i] < cost[j]:
-                return cost[i] + costSum(i+2, j+2)
-            else:
-                return cost[j] + costSum(i + 2, j + 2)
-        sum = 0
+        f1 = f2 = 0
+        for x in reversed(cost):
+            f1, f2 = x + min(f1, f2), f1
 
-        while True:
-            if len(cost) == 0:
-                break
-            if len(cost) == 1:
-                sum += cost.pop(0)
-                break
-            left = cost.pop(0)
-            right = cost.pop(1)
-
-
-        return sum
+        return min(f1, f2)
 
 
 cost1 = [10, 15, 20]
 cost2 = [1, 100, 1, 1, 1, 100, 1, 1, 100, 1]
 
-result = Solution().minCostClimbingStairs(cost1)
+result = Solution().minCostClimbingStairs(cost2)
 print(result)
